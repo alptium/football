89c2f81389d4786e52349f4322d6d221a20036ee
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-03-05 20:13:19 +0100

    Created class Player

---
+++ alptium-football/src/com/alptium/football/Player.java
@@ -0,0 +1,35 @@
+package com.alptium.football;
+
+public class Player {
+	
+	private String name;
+	private String surname;
+	private int age;
+	private String position;
+	
+	
+	public Player(String name, String surname,int age, String position) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.position = position;
+	}	
+	
+	
+		public String getName() {
+			return name;		
+		}
+		
+		public String getSur() {
+			return surname;		
+		}
+		
+		public int getAge() {
+			return age;		
+		}
+		
+		public String getPosition() {
+			return position;		
+		}
+}
+
