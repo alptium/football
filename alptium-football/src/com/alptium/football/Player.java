1b95d8551d9c049f97e1ff8eefa2aec72fd6efbf
lespabrandzulic82 <branko.spalovic@gmail.com>
2018-03-11 20:47:25 +0100

    Creating player class, add

--- alptium-football/src/com/alptium/football/Player.java
+++ alptium-football/src/com/alptium/football/Player.java
@@ -6,16 +6,17 @@ public class Player {
 	private String surname;
 	private int age;
 	private String position;
+	private String team;
 	
 	
-	public Player(String name, String surname,int age, String position) {
+	public Player(String name, String surname,int age, String position, String team) {
 		this.name = name;
 		this.surname = surname;
 		this.age = age;
 		this.position = position;
+		this.team = team;
 	}	
-	
-	
+		
 		public String getName() {
 			return name;		
 		}
@@ -30,6 +31,10 @@ public class Player {
 		
 		public String getPosition() {
 			return position;		
+		}
+		
+		public String getTeam() {
+			return team;		
 		}
 }
 
